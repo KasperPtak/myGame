bf660194f50f10ea5ef139c51e4a42ce10cb8b9e
KasperPtak <kasperptak2@gmail.com>
2024-01-31 23:50:05 +0100

    add health to units, weapon to player, damage anim for enemies

---
+++ src/entities.js
@@ -0,0 +1,97 @@
+export function createPlayer(k) {
+  const player = k.add([
+    k.pos(k.center()),
+    k.sprite("bean"),
+    k.area(),
+    k.body(),
+    k.health(10),
+    k.anchor("center"),
+  ]);
+
+  const sword = player.add([
+    k.sprite("master-sword"),
+    k.rotate(0),
+    k.anchor("bot"),
+    k.scale(3),
+    k.area(),
+    "sword",
+  ]);
+
+  player.onUpdate(() => {
+    sword.angle += 200 * k.dt();
+  });
+
+  return { player, sword };
+}
+
+export function createWalls(k) {
+  const wallHeight = 120;
+
+  const topWall = k.add([
+    k.pos(0, 0),
+    k.rect(k.width(), wallHeight),
+    k.outline(4),
+    k.area(),
+    k.body({ isStatic: true }),
+    "wall",
+  ]);
+
+  const bottomWall = k.add([
+    k.pos(0, k.height() - wallHeight),
+    k.rect(k.width(), wallHeight),
+    k.outline(4),
+    k.area(),
+    k.body({ isStatic: true }),
+  ]);
+
+  return { topWall, bottomWall };
+}
+
+export function spawnEnemy(k, player, topWall, bottomWall) {
+  const playerSpeed = 800;
+  const spawnSide = k.rand() < 0.5 ? -1 : 1;
+  const spawnX = player.pos.x + (spawnSide * k.width()) / 2 + k.rand(20, 50);
+  const spawnY = k.rand(bottomWall.pos.y - 120, topWall.pos.y - 120);
+
+  const enemy = k.add([
+    k.pos(spawnX, spawnY),
+    k.sprite("frog"),
+    k.anchor("center"),
+    k.scale(2.5),
+    k.body(),
+    k.area(scale(0.5)),
+    k.rotate(0),
+    k.health(10),
+    "enemy",
+    // k.area({
+    //     scale: 0.5,
+    //     collisionIgnore: 'enemy',
+    // }),
+  ]);
+
+  // TODO: somehow change enemy movement to not run on every frame
+  //   loop(.5, () => {
+  //     const direction = vec2(player.pos).sub(enemy.pos).unit();
+  //     enemy.move(direction.scale(playerSpeed / 4));
+  //   });
+
+  let isHopping = false;
+
+  player.onUpdate(() => {
+    const direction = vec2(player.pos).sub(enemy.pos).unit();
+    enemy.move(direction.scale(playerSpeed / 4));
+
+    if (enemy.pos.x > player.pos.x) {
+      enemy.scale.x = -Math.abs(enemy.scale.x); // Flip the enemy
+    } else {
+      enemy.scale.x = Math.abs(enemy.scale.x); // Reset scale
+    }
+    enemy.angle = Math.atan2(direction.y, direction.x);
+
+    if (!isHopping) {
+      enemy.play("hop");
+      isHopping = true;
+    }
+  });
+  return enemy;
+}
--- src/main.js
+++ src/main.js
@@ -1,32 +1,46 @@
 import kaboom from "kaboom";
+import { spawnEnemy, createWalls, createPlayer } from "./entities.js";
 
 const k = kaboom();
 
 const playerSpeed = 800;
-const wallHeight = 120;
+
+setBackground(100,100,100)
 
 k.loadSprite("bean", "sprites/bean.png");
+k.loadSprite("master-sword", "sprites/master-sword.png");
+k.loadSprite("frog", "sprites/BlueBlue/ToxicFrogBlueBlue_Sheet.png", {
+  sliceX: 9,
+  sliceY: 5,
+  anims: {
+	hop: {
+	  from: 0,
+	  to: 6,
+	  loop: true,
+	},
+	hurt: {
+		from: 26,
+		to: 30,
+	}
+  },
+});
 
-const player = k.add([k.pos(center()), k.sprite("bean"), k.area(), k.body()]);
+const { player, sword } = createPlayer(k);
+const { topWall, bottomWall } = createWalls(k);
 
-// topWall
-const topWall = k.add([
-  k.pos(0, 0),
-  k.rect(width(), wallHeight),
-  k.outline(4),
-  k.area(),
-  k.body({ isStatic: true }),
-  "wall",
+const currentHealth = add([
+  text("Health:" + player.hp()),
+  pos(width() / 2, 40),
+  color(0, 0, 255),
+  fixed(),
+  anchor("center"),
 ]);
 
-//bottomWall
-const bottomWall = k.add([
-  k.pos(0, height() - wallHeight),
-  k.rect(width(), wallHeight),
-  k.outline(4),
-  k.area(),
-  k.body({ isStatic: true }),
-]);
+const enemies = []; // Array to store spawned enemies
+k.loop(rand(0.5, 1.5), () => {
+  const enemy = spawnEnemy(k, player, topWall, bottomWall);
+  enemies.push(enemy); // Add the spawned enemy to the array
+});
 
 onKeyDown("left", () => {
   player.move(-playerSpeed, 0);
@@ -41,41 +55,62 @@ onKeyDown("down", () => {
   player.move(0, playerSpeed);
 });
 
-function spawnEnemy() {
-  const spawnSide = k.rand() < 0.5 ? -1 : 1; // Randomly choose left (-1) or right (1) side
-  const spawnX = player.pos.x + (spawnSide * k.width()) / 2 + k.rand(20, 50); // Dynamic spawnX based on player's x position and chosen side
-  const spawnY = k.rand(bottomWall.pos.y - 120, topWall.pos.y - 120); // Random Y position within the walls
-  const enemy = k.add([
-    k.pos(spawnX, spawnY),
-    k.sprite("bean"),
-    k.area(),
-    k.body(),
-    "enemy",
-  ]);
-
-  player.onUpdate(() => {
-    const direction = vec2(player.pos).sub(enemy.pos).unit();
-    enemy.move(direction.scale(playerSpeed / 4));
-  });
-}
-
-k.loop(1, () => {
-  spawnEnemy();
-});
-
 player.onUpdate(() => {
   k.camPos(player.pos.x, height() / 2);
 
+  //TODO: change this to check on player y pos instead of moving walls
   bottomWall.pos.x = player.pos.x - width() / 2;
   topWall.pos.x = player.pos.x - width() / 2;
+});
 
-  player.onCollide("enemy", () => {
-    debug.log("leave grass");
-    go("defeat");
-    debug.log(player.pos.x);
+function moveEnemySmoothly(enemy, targetX, duration) {
+    let curTween = null
+    // Stop previous tween, if any
+    if (curTween) curTween.cancel();
+    
+    // Start the tween animation
+    curTween = tween(
+        // Start value
+        enemy.pos.x,
+        // Destination value
+        enemy.pos.x + targetX,
+        // Duration (in seconds)
+        duration,
+        // Update function
+        (val) => enemy.pos.x = val,
+        // Interpolation function (defaults to easings.linear)
+        easings['easeOutQuad'],
+    );
+}
+sword.onCollide("enemy", (enemy) => {
+	debug.log("Sword collided with an enemy!");
+	enemy.hurt(1);
+	enemy.play('hurt')
+	if (enemy.pos.x < player.pos.x) {
+        debug.log('move left');
+        moveEnemySmoothly(enemy, -200, 0.3);
+    } else {
+        moveEnemySmoothly(enemy, +200, 0.3);
+    }
+
+	if (enemy.hp() <= 0) {
+	  destroy(enemy); 
+	  enemies.splice(enemies.indexOf(enemy), 1);
+	}
   });
 
-  scene("defeat", () => {
-    add([text("You Lose!"), pos(width() / 2, height() / 2), anchor("center")]);
-  });
+player.onCollide("enemy", () => {
+  player.hurt(3);
+});
+
+player.onHurt(() => {
+  currentHealth.text = "Health:" + player.hp();
+});
+
+player.onDeath(() => {
+  go("defeat");
+});
+
+k.scene("defeat", () => {
+  add([text("You Lose!"), pos(width() / 2, height() / 2), anchor("center")]);
 });
